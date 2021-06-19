da83f11d740802c0e8945993c41736476d62e479
Gia Phat Tram <giaphattram@gmail.com>
2021-06-20 00:35:20 +0200

    Add files via upload

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -33,6 +33,63 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_all_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['categories'])
+
+    def test_get_all_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['categories'])
+
+    def test_add_new_questions(self):
+        input_question = 'New Question 1'
+        input_answer = 'New Answer 1'
+        input_category = 1
+        input_difficulty = 4
+        new_question = Question(question = input_question, answer = input_answer, category = input_category, difficulty = input_difficulty)
+
+        res = self.client().post('/questions', json=new_question.format())
+
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['question'], input_question)
+        self.assertEqual(data['answer'], input_answer)
+        self.assertEqual(data['category'], input_category)
+        self.assertEqual(data['difficulty'], input_difficulty)
+
+    def test_search_questions(self):
+        search_term = {'searchTerm': 'what'}
+        res = self.client().post('/questions/search', json=search_term)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['totalQuestions'])
+
+    def test_get_questions_by_category(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['totalQuestions'])
+
+    def test_404_get_questions_from_non_existing_category(self):
+        res = self.client().get('/categories/1000000/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], "Not found")
 
 
 # Make the tests conveniently executable
